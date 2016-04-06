9df0630e058ae3c8ae4bda27d18d6f27a91698dd
Seed <seed@ubuntu.(none)>
2016-04-06 09:51:11 -0700

    works

--- vpn_server/server_config.sh
+++ vpn_server/server_config.sh
@@ -1,3 +1,3 @@
-ip addr add 10.0.4.1/24 dev tun0 # server tun ip
-ifconfig tun0 up
-route add -net 10.0.5.0/24 dev tun0 
+ip addr add 10.0.4.1/24 dev toto0 # server tun ip
+ifconfig toto0 up
+route add -net 10.0.5.0/24 dev toto0 
