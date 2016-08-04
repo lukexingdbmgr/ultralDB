65ec08143934ff1a6e98631edf771d2bd964cc54
xingm <xingm@DESKTOP-C9LNENH>
2016-08-03 22:03:22 -0700

    test for java algo

---
+++ vpn_server/testjava.java
@@ -0,0 +1,13 @@
+package testingone;
+
+public class element{
+	int a; 
+	int b;
+	public element(int a, int b){
+		this.a = a;
+		this.b = b;
+	}
+	public void print_out(){
+		System.out.println("a + b"  + a + b);
+	}   
+};  
